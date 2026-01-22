a024e49be879f80ae2517e03d26094edcc5fd575
DUB1401 <vlad.milosta@yandex.ru>
2026-01-22 21:18:16 +0300

    Add: header parser

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Source.Core.Base.Formats.Ranobe import Branch, Chapter, ChaptersTypes, Ranobe
+from Source.Core.Base.Formats.Ranobe import Branch, Chapter, ChapterHeaderParser
 from Source.Core.Base.Parsers.RanobeParser import RanobeParser
 from Source.Core.Base.Formats.BaseFormat import Statuses
 
@@ -7,105 +7,27 @@ from dublib.Polyglot import HTML
 
 from time import sleep
 
-from recognizers_number import recognize_number, Culture
 from bs4 import BeautifulSoup
 
 class Parser(RanobeParser):
 	"""Парсер."""
-	
+
 	#==========================================================================================#
-	# >>>>> ПЕРЕОПРЕДЕЛЯЕМЫЕ МЕТОДЫ <<<<< #
+	# >>>>> ПРИВАТНЫЕ МЕТОДЫ <<<<< #
 	#==========================================================================================#
 
-	def __BuildImageLink(self, src: str) -> str:
+	def __BuildFullLink(self, link: str) -> str:
 		"""
-		Строит полную ссылку из источника.
+		Строит полную ссылку.
 
-		:param src: Источник изображения.
-		:type src: str
+		:param link: Ссылка.
+		:type link: str
 		:return: Полная ссылка.
 		:rtype: str
 		"""
 
-		if src.startswith("http"): return src
-		return f"https://{self._Manifest.site}/" + src.lstrip("/")
-
-	def __ReplaceNumberFromChapterName(self, name: str, number: str) -> str:
-		"""
-		Уадляет часть с номером главы/тома из названия.
-
-		:param name: Полное название главы.
-		:type name: str
-		:param number: Номер главы или тома для разбивки.
-		:type number: str
-		:return: Обработанное название главы.
-		:rtype: str
-		"""
-
-		if number:
-			Buffer = list()
-
-			Buffer = name.split(number)
-			Buffer = Buffer[1:]
-			name = number.join(Buffer)
-			name = name.strip()
-
-			if name and not name[0].isalpha(): name = name.lstrip("-.–")
-
-		return name
-
-	def __CheckChapterType(self, fullname: str, name: str) -> ChaptersTypes | None:
-		"""
-		Пытается определить тип главы.
-
-		:param fullname: Полное название главы.
-		:type fullname: str
-		:param name: Часть названия главы без номера тома и номера главы.
-		:type name: str
-		:return: Тип главы.
-		:rtype: ChaptersTypes | None
-		"""
-
-		fullname = fullname.lower()
-		name = name.lower()
-
-		#---> afterword
-		#==========================================================================================#
-		if "послесловие" in name: return ChaptersTypes.afterword
-
-		#---> art
-		#==========================================================================================#
-		if name.startswith("начальные") and "иллюстрации" in name: return ChaptersTypes.art
-
-		#---> epilogue
-		#==========================================================================================#
-		if "эпилог" in name: return ChaptersTypes.epilogue
-
-		#---> extra
-		#==========================================================================================#
-		if name.startswith("дополнительн") and "истори" in name: return ChaptersTypes.extra
-		if name.startswith("бонус") and "истори" in name: return ChaptersTypes.extra
-		if name.startswith("экстра"): return ChaptersTypes.extra
-		if "глава" not in fullname and "том" in fullname: return ChaptersTypes.extra
-
-		#---> glossary
-		#==========================================================================================#
-		if name.startswith("глоссарий"): return ChaptersTypes.glossary
-
-		#---> prologue
-		#==========================================================================================#
-		if "пролог" in name: return ChaptersTypes.prologue
-
-		#---> trash
-		#==========================================================================================#
-		if name.startswith("реквизиты") and "переводчик" in name: return ChaptersTypes.trash
-		if name.startswith("примечани") and "переводчик" in name: return ChaptersTypes.trash
-
-		#---> chapter
-		#==========================================================================================#
-		if "глава" in fullname: return ChaptersTypes.chapter
-
-		return None
+		if link.startswith("http"): return link
+		return f"https://{self._Manifest.site}/" + link.lstrip("/")
 
 	def __GetFromSlugID(self) -> int | None:
 		"""
@@ -117,6 +39,47 @@ class Parser(RanobeParser):
 
 		try: return int(self._Title.slug.split("-")[0])
 		except ValueError: pass
+
+	def __GetParagraphs(self, chapter: Chapter) -> tuple[str]:
+		"""
+		Получает абзацы в главе.
+
+		:param chapter: Данные главы.
+		:type chapter: Chapter
+		:return: Последовательность абзацев главы.
+		:rtype: tuple[str]
+		"""
+
+		Paragraphs = list()
+		Response = self._Requestor.get(f"https://{self._Manifest.site}/chapters/{self._Title.slug}/{chapter.slug}.html")
+
+		if Response.status_code == 200:
+			Soup = BeautifulSoup(Response.text, "lxml")
+			Container = Soup.find("div", {"id": "arrticle"})
+
+			Scripts = Container.find_all("script")
+			for Script in Scripts: Script.decompose()
+			ParagraphsBlocks = Container.find_all("p", recursive = False)
+
+			for Index in range(len(ParagraphsBlocks)):
+				ImagesBlocks = ParagraphsBlocks[Index].find_all("img")
+
+				for Div in ParagraphsBlocks[Index].find_all("div", {"align": "center"}): Div.decompose()
+
+				if not ParagraphsBlocks[Index].get_text().strip() and ImagesBlocks:
+					Images = "".join(tuple(f"<img src=\"" + self.__BuildFullLink(Block["src"]) + "\">" for Block in ImagesBlocks))
+					ParagraphsBlocks[Index] = BeautifulSoup(f"<p>{Images}</p>", "html.parser")
+
+				elif ImagesBlocks:
+					for Block in ImagesBlocks:
+						Block.attrs = {"src": self.__BuildFullLink(Block["src"])}
+
+				Paragraphs.append(str(ParagraphsBlocks[Index]))
+			
+		elif Response.status_code == 404: self._Portals.chapter_not_found(self._Title, chapter)
+		else: self._Portals.request_error(Response, "Unable request chapter.")
+
+		return Paragraphs
 
 	def __ParseCover(self, soup: BeautifulSoup):
 		"""
@@ -236,7 +199,7 @@ class Parser(RanobeParser):
 		ChaptersLink = soup.find("a", {"title": "Перейти в оглавление"})["href"]
 
 		while True:
-			Response = self._Requestor.get(f"{ChaptersLink}page/{Page}/")
+			Response = self._Requestor.get(self.__BuildFullLink(f"{ChaptersLink}page/{Page}/"))
 			Soup = BeautifulSoup(Response.text, "html.parser")
 
 			if Response.status_code == 200:
@@ -244,36 +207,19 @@ class Parser(RanobeParser):
 				
 				for Line in Lines:
 					Link: str = Line.find("a")["href"]
-					Name: str = Line.find("h6").get_text()
+					ChapterHeader: str = Line.find("h6").get_text()
 					ChapterSlug = Link.split("/")[-1][:-5]
 					ChapterID = int(ChapterSlug.split("-")[0])
-
-					Results = recognize_number(Name, Culture.English)
-
-					ChapterVolume = None
-					ChapterNumber = None
-
-					for Index in range(len(Results)):
-						NameWords = tuple(Part.lower() for Part in Name.split())
-						HasVolume = "том" in NameWords
-						HasChapter = "глава" in NameWords
-
-						if Index == 0 and HasVolume: ChapterVolume = Results[Index].resolution["value"]
-						elif HasChapter: ChapterNumber = Results[Index].resolution["value"]
-
-						Index += 1
-
-					if ChapterVolume or ChapterNumber: Name = self.__ReplaceNumberFromChapterName(Name, ChapterNumber or ChapterVolume)
+					HeaderData = ChapterHeaderParser(ChapterHeader, self._Title.words_dictionary).parse()
 
 					Buffer = Chapter(self._SystemObjects, self._Title)
 					Buffer.set_id(ChapterID)
 					Buffer.set_slug(ChapterSlug)
-					Buffer.set_name(Name)
-					Buffer.set_number(ChapterNumber)
-					Buffer.set_volume(ChapterVolume)
-					Buffer.set_type(self.__CheckChapterType(Line.find("h6").get_text(), Name))
+					Buffer.set_name(HeaderData.name)
+					Buffer.set_number(HeaderData.number)
+					Buffer.set_volume(HeaderData.volume)
+					Buffer.set_type(HeaderData.type)
 					Buffer.set_is_paid(False)
-
 					BranchObject.add_chapter(Buffer)
 
 				self._Portals.info(f"Chapters data collected on page {Page}.")
@@ -284,47 +230,6 @@ class Parser(RanobeParser):
 			else: self._Portals.request_error(Response, "Unable request chapters page.")
 
 		self._Title.add_branch(BranchObject)
-
-	def __GetParagraphs(self, chapter: Chapter) -> tuple[str]:
-		"""
-		Получает абзацы в главе.
-
-		:param chapter: Данные главы.
-		:type chapter: Chapter
-		:return: Последовательность абзацев главы.
-		:rtype: tuple[str]
-		"""
-
-		Paragraphs = list()
-		Response = self._Requestor.get(f"https://{self._Manifest.site}/chapters/{self._Title.slug}/{chapter.slug}.html")
-
-		if Response.status_code == 200:
-			Soup = BeautifulSoup(Response.text, "html.parser")
-			Container = Soup.find("div", {"id": "arrticle"})
-
-			Scripts = Container.find_all("script")
-			for Script in Scripts: Script.decompose()
-			ParagraphsBlocks = Container.find_all("p", recursive = False)
-
-			for Index in range(len(ParagraphsBlocks)):
-				ImagesBlocks = ParagraphsBlocks[Index].find_all("img")
-
-				for Div in ParagraphsBlocks[Index].find_all("div", {"align": "center"}): Div.decompose()
-
-				if not ParagraphsBlocks[Index].get_text().strip() and ImagesBlocks:
-					Images = "".join(tuple(f"<img src=\"" + self.__BuildImageLink(Block["src"]) + "\">" for Block in ImagesBlocks))
-					ParagraphsBlocks[Index] = BeautifulSoup(f"<p>{Images}</p>", "html.parser")
-
-				elif ImagesBlocks:
-					for Block in ImagesBlocks:
-						Block.attrs = {"src": self.__BuildImageLink(Block["src"])}
-
-				Paragraphs.append(str(ParagraphsBlocks[Index]))
-			
-		elif Response.status_code == 404: self._Portals.chapter_not_found(self._Title, chapter)
-		else: self._Portals.request_error(Response, "Unable request chapter.")
-
-		return Paragraphs
 
 	#==========================================================================================#
 	# >>>>> ПУБЛИЧНЫЕ МЕТОДЫ <<<<< #
@@ -373,4 +278,3 @@ class Parser(RanobeParser):
 
 		elif Response.status_code == 404: self._Portals.title_not_found(self._Title)
 		else: self._Portals.request_error(Response, "Unable request title page.")
-
